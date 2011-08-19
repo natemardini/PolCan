3089a63b0ee89c0abf0a7e0f3bda98c9462b33f3
Chad Yehia <cyehia@gmail.com>
2011-08-18 20:53:18 -0400

    one more tinymce

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,13 @@
 class PartiesController < ApplicationController
   before_filter :authenticate_member!
   
+  uses_tiny_mce :options => {
+                              :forced_root_block => false,
+                              :force_br_newlines => true,
+                              :force_p_newlines => false,
+                              :theme_advanced_resizing => true,
+                            }
+  
   def caucus
     @party = current_member.party
     @discussions = @party.forum.discussions.order('updated_at DESC')
